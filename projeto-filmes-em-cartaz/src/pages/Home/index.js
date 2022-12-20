58d0622883b12df6163094829429fd878c2a73f8
gabriel-maximus <gabrielmaximus80@gmail.com>
2022-12-20 17:08:03 -0300

    Ajustes nos estilos

--- projeto-filmes-em-cartaz/src/pages/Home/index.js
+++ projeto-filmes-em-cartaz/src/pages/Home/index.js
@@ -44,6 +44,7 @@ export default function Home() {
               <strong>{filme.title}</strong>
               <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={`Poster do filme ${filme.title}`} />
               <Link to={`/filme/${filme.id}`}>Acessar</Link>
+              <hr />
             </article>
           )
         })}
